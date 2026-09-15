5febda1465c745644a46037dd6825d110bc08e45
Gaston Jaureguiberry <gaston.jaureguib@gmail.com>
2026-09-15 10:17:04 -0300

    fix styles folder casing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { HiloProvider } from "./context/HiloContext.jsx";
 
-import "./styles/global.css";
+import "./Styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
