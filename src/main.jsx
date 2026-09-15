62431de52c41a58645187cb6982839000242efc5
Gaston Jaureguiberry <gaston.jaureguib@gmail.com>
2026-09-15 10:25:49 -0300

    resuelto

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { HiloProvider } from "./context/HiloContext.jsx";
 
-import "./Styles/global.css";
+import "./styles/global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
